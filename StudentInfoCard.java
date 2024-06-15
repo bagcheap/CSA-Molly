38bd44f107291c7076eed0382838146066621ebf
angus <spare_brain@yahoo.com>
2024-06-15 11:49:47 -0400

    Working code

---
+++ StudentInfoCard.java
@@ -0,0 +1,47 @@
+/**
+ *
+ * @author Molly Bagchee
+ * @version 2024-06-01
+ * @description Assignment 01.04 (CSA) - Provides key Info on Student
+ * 
+ */
+public class StudentInfoCard {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("****************************************************************");
+        System.out.println("*                                                              *");
+        System.out.println("*                    Student Information                       *");
+        System.out.println("*                                                              *");
+        System.out.println("****************************************************************");
+        
+        System.out.print("Name: Molly Bagchee");
+        System.out.println("        Birthday: March 15, 2008");
+        System.out.print("Grade: 11");
+        System.out.println("                  Age: 15 years"); //
+        System.out.println("");
+
+        System.out.print("School: Seminole High School");
+        System.out.println("    City: Sanford, FL");
+        System.out.println(" ");
+
+        System.out.println("Guardian:");
+        System.out.println("Name: Rupa Bagchee");
+
+        System.out.print("Home Phone: (407)-462-2587");
+        System.out.println("    Availability: During daytime hours");
+        System.out.print("Cell Phone: (407)-462-2587");
+        System.out.println("    Availability: During the day");
+        System.out.println("Email: rbagchee@gmail.com");
+        System.out.println(" ");
+
+        System.out.println("Highest Math Course Taken: AP Algebra 1");
+        System.out.println(" ");
+
+        System.out.println("Computer Experience:");
+        System.out.println("Experience in coding VEX robots using C and Scratch (from MIT). Also programmed a little in Python and Java.");
+        System.out.println(" ");
+        System.out.print("Reason for taking this course: ");
+        System.out.println("Use programming for solving machine language (ML). Learning Python and R.");
+    }
+
+}
